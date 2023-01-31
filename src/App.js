0fc1c2347b64c0b1115c2f4f7baf4ee80fa19ab8
HughesSeanKyle <khughessean@yahoo.com>
2023-01-31 13:47:43 +0200

    Sign in form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import signUpImage from './assets/images/signup-image.jpg';
+
 function App() {
-	return <div className="text-3xl font-bold underline">Hello</div>;
+	return (
+		<div className="bg-custom-green grid grid-cols-2 sm:grid-cols-2 h-screen w-full">
+			<div className="flex flex-col justify-center">
+				<form className="max-w-[400px] w-full mx-auto rounded-lg shadow-lg shadow-custom-black bg-gray-900 p-8 px-8">
+					<h2 className="text-4xl text-custom-white font-bold text-center">
+						SIGN IN
+					</h2>
+					<div className="flex flex-col text-custom-white py-2">
+						<label>Username</label>
+						<input
+							className="rounded-lg bg-custom-white mt-2 p-2 focus:border-blue-900 focus:outline-none focus:ring focus:ring-custom-gray"
+							type="text"
+						/>
+					</div>
+					<div className="flex flex-col text-custom-white py-2">
+						<label>Password</label>
+						<input
+							className="p-2 rounded-lg bg-custom-white mt-2 focus:border-blue-900 focus:outline-none focus:ring focus:ring-custom-gray"
+							type="password"
+						/>
+					</div>
+					<div className="flex justify-between text-custom-white py-2">
+						<p className="flex items-center">
+							<input className="mr-2" type="checkbox" /> Remember Me
+						</p>
+						<p>Forgot Password</p>
+					</div>
+					<button className="w-full my-5 py-2 bg-custom-green shadow-md shadow-custom-gray text-white font-semibold rounded-lg hover:shadow-md hover:shadow-custom-white hover:bg-custom-green-500">
+						SIGNIN
+					</button>
+				</form>
+			</div>
+			<div className="hidden sm:block shadow-lg inset-0 rounded-l-lg shadow-lg shadow-custom-gray">
+				<img
+					className="rounded-l-sm w-full h-screen object-cover"
+					src={signUpImage}
+					alt=""
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'test-col': '#32a852',
+				'custom-green': '#0F2527',
+				'custom-black': '#0B211F',
+				'custom-gray': '#4A656C',
+				'custom-white': '#DDDEE0',
+			},
+		},
 	},
 	plugins: [],
 };
