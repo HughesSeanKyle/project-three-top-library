4c1c4b7f82bbf813689d61ca51f0e2c62f8a016e
HughesSeanKyle <khughessean@yahoo.com>
2023-02-01 13:31:06 +0200

    Signup UI complete

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -6,7 +6,7 @@ const SignUp = () => {
 	return (
 		<div className="bg-custom-green grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 h-screen w-full">
 			<div className="flex flex-col justify-center content-center items-center form-padding image-bg-mobile-only dark-layer">
-				<h2 className="text-4xl text-custom-white font-bold text-center mb-5 mobile-z-index">
+				<h2 className="text-4xl text-custom-white font-bold text-center my-5  mobile-z-index">
 					BookWorks.io
 				</h2>
 				<form className="max-w-[400px] w-full mx-auto rounded-lg shadow-lg shadow-custom-black bg-gray-900 p-8 px-8 mobile-z-index">
@@ -21,7 +21,21 @@ const SignUp = () => {
 						/>
 					</div>
 					<div className="flex flex-col text-custom-white py-2">
+						<label>Email</label>
+						<input
+							className="rounded-lg bg-custom-white mt-2 p-2 focus:border-blue-900 focus:outline-none focus:ring focus:ring-custom-gray"
+							type="email"
+						/>
+					</div>
+					<div className="flex flex-col text-custom-white py-2">
 						<label>Password</label>
+						<input
+							className="p-2 rounded-lg bg-custom-white mt-2 focus:border-blue-900 focus:outline-none focus:ring focus:ring-custom-gray"
+							type="password"
+						/>
+					</div>
+					<div className="flex flex-col text-custom-white py-2">
+						<label>Confirm Password</label>
 						<input
 							className="p-2 rounded-lg bg-custom-white mt-2 focus:border-blue-900 focus:outline-none focus:ring focus:ring-custom-gray"
 							type="password"
@@ -36,7 +50,7 @@ const SignUp = () => {
 						SIGN UP
 					</button>
 				</form>
-				<div className="max-w-[360px] flex flex-wrap mt-6 relative w-2/3 mobile-width-reset text-custom-white font-semibold">
+				<div className="max-w-[360px] flex flex-wrap mt-1 mb-3 relative w-2/3 mobile-width-reset text-custom-white font-semibold">
 					<div className="w-1/2">
 						<Link to="/auth/signin">
 							<small>Already have an account?</small>
