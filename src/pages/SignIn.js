71274bd870a0a678d33baebc3d5c6ec6ec4450e4
HughesSeanKyle <khughessean@yahoo.com>
2023-02-07 09:32:52 +0200

    Update

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -45,7 +45,7 @@ const SignIn = () => {
 						SIGN IN
 					</h2>
 					<div className="flex flex-col text-custom-white py-2">
-						<label>Username</label>
+						<label>Username.</label>
 						<input
 							className="rounded-lg bg-custom-white mt-2 p-2 focus:border-blue-900 focus:outline-none focus:ring focus:ring-custom-gray text-custom-black"
 							type="text"
